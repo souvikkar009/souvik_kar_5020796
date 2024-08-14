20977eb73fec237af5937b7b7cf18e6f01039655
Souvik Kar <souvikkar009@gmail.com>
2024-08-15 00:09:35 +0530

    exercise 6 added

---
+++ Week_3/Exercise_6 - EMS - Implementing Pagination and Sorting/EmployeeManagementSystem/src/main/java/com/ems/services/implementations/DepartmentServiceImplementations.java
@@ -0,0 +1,61 @@
+package com.ems.services.implementations;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import com.ems.entitites.Department;
+import com.ems.repositories.DepartmentRepository;
+import com.ems.services.DepartmentServices;
+
+@Service
+public class DepartmentServiceImplementations implements DepartmentServices {
+
+	@Autowired
+	private DepartmentRepository departmentRepository;
+
+	@Override
+	public Department addDepartment(Department department) {
+		return departmentRepository.save(department);
+	}
+
+	@Override
+	public String deleteDepartment(int departmentId) {
+		departmentRepository.deleteById(departmentId);
+		return "Department with department id: " + departmentId + " is deleted.";
+	}
+
+	@Override
+	public Department updateDepartment(Department department) {
+		departmentRepository.save(department);
+		return department;
+	}
+
+	@Override
+	public Department getDepartmentById(int departmentId) {
+		return departmentRepository.findById(departmentId).get();
+	}
+
+	@Override
+	public Department getDepartmentByName(String name) {
+		return departmentRepository.findByName(name);
+	}
+
+	@Override
+	public List<Department> getAllDepartment() {
+		List<Department> departments = new ArrayList<>();
+		departmentRepository.findAll().forEach(department -> departments.add(department));
+		return departments;
+	}
+
+	@Override
+	public Page<Department> searchDepartmentByName(String nameFragemnt, Pageable pageable) {
+		Page<Department> departments = departmentRepository.findByNameContaining(nameFragemnt, pageable);
+		return departments;
+	}
+
+}
---
+++ Week_3/Exercise_6 - EMS - Implementing Pagination and Sorting/EmployeeManagementSystem/src/main/java/com/ems/services/implementations/EmployeeServiceImplementations.java
@@ -0,0 +1,93 @@
+package com.ems.services.implementations;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import com.ems.dtos.EmployeeDto;
+import com.ems.entitites.Department;
+import com.ems.entitites.Employee;
+import com.ems.repositories.DepartmentRepository;
+import com.ems.repositories.EmployeeRepository;
+import com.ems.services.EmployeeServices;
+
+@Service
+public class EmployeeServiceImplementations implements EmployeeServices{
+	
+	@Autowired
+	private EmployeeRepository employeeRepository;
+	
+	@Autowired
+	private DepartmentRepository departmentRepository;
+
+	@Override
+	public Employee addEmployee(EmployeeDto employeeDto) {
+		Department department = departmentRepository.findById(employeeDto.getDeptId()).orElse(null);
+		Employee employee = new Employee();
+		employee.setName(employeeDto.getName());
+		employee.setEmail(employeeDto.getEmail());
+		employee.setDepartment(department);
+		employeeRepository.save(employee);
+		return employee;
+	}
+
+	@Override
+	public String deleteEmployee(int employeeId) {
+		employeeRepository.deleteById(employeeId);
+		return "Employee with id: " + employeeId + " is deleted.";
+	}
+
+	@Override
+	public Employee updateEmployee(EmployeeDto employeeDto) {
+		Department department = departmentRepository.findById(employeeDto.getDeptId()).orElse(null);
+		Employee employee = new Employee();
+		employee.setId(employeeDto.getId());
+		employee.setName(employeeDto.getName());
+		employee.setEmail(employeeDto.getEmail());
+		employee.setDepartment(department);
+		employeeRepository.save(employee);
+		return employee;
+	}
+
+	@Override
+	public Employee getEmployeeById(int employeeId) {
+		return employeeRepository.findById(employeeId).get();
+		
+	}
+
+	@Override
+	public List<Employee> getEmployeesByName(String name) {
+		List<Employee> employees = new ArrayList<>();
+		employeeRepository.findByName(name).forEach(employee -> employees.add(employee));
+		return employees;
+	}
+
+	@Override
+	public Employee getEmployeeByEmail(String email) {
+		return employeeRepository.findByEmail(email);
+	}
+
+	@Override
+	public List<Employee> getAllEmployees() {
+		List<Employee> employees = new ArrayList<>();
+		employeeRepository.findAll().forEach(employee -> employees.add(employee));
+		return employees;
+	}
+
+	@Override
+	public Page<Employee> searchEmployeeByName(String nameFragemnt, Pageable pageable) {
+		Page<Employee> employees = employeeRepository.findByNameContaining(nameFragemnt, pageable);
+		return employees;
+	}
+
+	@Override
+	public Page<Employee> searchEmployeeByEmail(String emailFragemnt, Pageable pageable) {
+		Page<Employee> employees = employeeRepository.findByEmailContaining(emailFragemnt, pageable);
+		return employees;
+	}
+
+}
